eee3cf9e4eb05b7dc2f6a7ff15621593f3172936
Ethan-Amsden <eamsden2@edisonohio.edu>
2023-10-04 13:25:55 -0400

    4th commit;
    Updated project by adding in a custom helper and started on videon 12/16 located here:
    https://www.youtube.com/watch?v=2sXjmewEQOY&list=PLtV5RF44Yj8S4RcpQehL-2XMuVsJXwNvK

--- JS/characters.js
+++ JS/characters.js
@@ -1,38 +1,47 @@
-var cast = {
-    "characters": [
-        {
-            "name": "Toothless",
-            "shortCode": "toothless.jpg",
-            "home": "berk",
-            "location": null
-        },
-        {
-            "name": "Viggo Grimborne",
-            "shortCode": "viggo-grimborn.jpg",
-            "home": "unknown",
-            "location": "on a ship"
-        },
-        {
-            "name": "Zuko",
-            "shortCode": "zuko.jpg",
-            "home": "Fire Nation",
-            "location": null
-        },
-        {
-            "name": "Aang",
-            "shortCode": "avatar_aang.png",
-            "home": "Southern Air Temple",
-            "location": "in the sky with Opa"
-        }
-    ]
-};
+// Found on stackoverflow.com
+// get query string parameter
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
+// Custom helper 1: expression
+Handlebars.registerHelper("emailFrmt", function(prop) {
+    if (prop) {
+        return new Handlebars.SafeString("<strong>" + prop + "</strong>");
+    }
+});
+
+// Custom Helper 2: block
+Handlebars.registerHelper("makeItalic", function(props) {
+    return new Handlebars.SafeString("<i>" + props.fn(this) + "</i>");
+});
+
+// JQuery Document loader
 $(document).ready(function(){
 
     var characterTemplate = $("#character-template").html();
 
     var compiledCharTemp = Handlebars.compile(characterTemplate);
-    //console.log(compiledCharTemp(cast.characters[0]));
 
-    $(".character-list-container").html(compiledCharTemp(cast));//.characters[3]));
+    var characterId = getParameterByName("id");
+    console.log("character id: ", characterId);
+
+    $.ajax("./data/cast.json").done(function(cast){
+        //console.log(cast);
+        $(".character-list-container").html(compiledCharTemp(cast));
+
+        
+    });
+
+    $(".character-list-container").on("click", ".view-details", function(e) {
+        e.preventDefault();
+        console.log("expand details");
+    });
+    
 });
